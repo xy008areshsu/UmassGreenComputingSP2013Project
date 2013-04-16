eef1930ba2d0ac832271e99cc9d2440c2f28814b
xy008areshsu <xy008areshsu@gmail.com>
2013-04-16 16:19:02 -0400

    Finished preemptibleLoads

--- src/deferableLoads.m
+++ src/deferableLoads.m
@@ -1,14 +1,14 @@
 % Deferable Loads Modeling: [relative deadline in hours, period in hours, 
-% excution time in hours, power per period in kWh, relative starting point 
-% within each period in hours]
+% excution time in hours, power per period in kWh]
 
-% Assume period is integer, and can be divisible by T, no residual
-ACCentral = [24, 24, 8, 56, 12];
-refregerator = [2, 2, 2, 0.36, 0];
+% Assume period is integer, and can be divisible by T, no residual, and
+% deadline is equal to period
+ACCentral = [24, 24, 8, 56];
+refregerator = [2, 2, 1, 0.36];
 
-dishWasher = [24, 24, 2, 4, 0];
-clothesWasher = [24, 24, 0.8, 7, 0];
-clothesDryer = [24, 24, 1.5, 5, 0];
+dishWasher = [24, 24, 2, 4];
+clothesWasher = [24, 24, 0.8, 7];
+clothesDryer = [24, 24, 1.5, 5];
 
 nonPreemptibleLoads = [dishWasher; clothesWasher; clothesDryer];
 preemptibleLoads = [ACCentral; refregerator];
