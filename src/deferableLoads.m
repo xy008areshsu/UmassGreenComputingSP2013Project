d82dc40a11f5c211c388de7029fa358f0e2ca329
xy008areshsu <xy008areshsu@gmail.com>
2013-07-13 22:44:10 -0400

    added shift load benefit

--- src/deferableLoads.m
+++ src/deferableLoads.m
@@ -1,10 +1,10 @@
 % Deferable Loads Modeling: [relative deadline in hours, period in hours, 
-% excution time in hours, power per period in kWh]
+% excution time in hours, energy per period in kWh]
 
 % Assume period is integer, and can be divisible by T, no residual, and
 % deadline is equal to period
-ACCentral = [24, 24, 8, 56];
-refregerator = [2, 2, 1, 0.36];
+ACCentral = [24, period, 8, 56 / (24 / period)];
+%refregerator = [2, 2, 1, 0.36];
 
 dishWasher = [24, 24, 2, neededPower];
 clothesWasher = [24, 24, 0.8, 7];
@@ -12,4 +12,4 @@ clothesDryer = [24, 24, 1.5, 5];
 %hahaha = [24, 3, 1.5, 5];
 
 nonPreemptibleLoads = [dishWasher; clothesWasher; clothesDryer];
-preemptibleLoads = [ACCentral; refregerator];
+preemptibleLoads = [ACCentral];
--- src/shiftBatch.m
+++ src/shiftBatch.m
@@ -7,22 +7,21 @@ GridCost = [2.7; 2.4; 2.3; 2.3; 2.3; 2.5; 2.8; 3.4; 3.8; 5; 6.1; 6.8; 7.4;
         
 %% Job 1, Fixed nonDeferablePower
 T = 24;
-Load = zeros(T, 1);
+nonDeferLoad = zeros(T, 1);
 neededPower = 4;
-period = 24;
 mergeData
 sss = size(LoadTotal);
 sss = sss(2);
 price = zeros(3, sss);
-Capa = 10:5:100;
-costBenifitForDiffCapa = zeros(size(Capa, 2), 1);
+periods = [1 2 3 4 6 8 12 24];
+costBenifitForDiffDutyCycles = zeros(size(periods, 2), 1);
 jjj = 1;
 
 
-for C = Capa
+for period = periods
     for iii = 1 : sss
-        Load = LoadTotal(:, iii);
-        storage
+        nonDeferLoad = LoadTotal(:, iii);
+        shift
         price(1, iii) = originalPrice;    
         price(2, iii) = cost;    %cost after optimization
         price(3, iii) = (originalPrice - cost) / originalPrice * 100;  %cost reduction
@@ -30,22 +29,20 @@ for C = Capa
     totalOriginalPrice = sum(price(1, :));
     totalOptimizedCost = sum(price(2, :));
     avgReduction = (totalOriginalPrice - totalOptimizedCost) / totalOriginalPrice * 100;
-    costBenifitForDiffCapa(jjj, 1) = avgReduction;
+    costBenifitForDiffDutyCycles(jjj, 1) = avgReduction;
     jjj = jjj + 1;
 end
 
 
-storageSimResultData = [Capa' costBenifitForDiffCapa];
+shiftSimResultData = [periods' costBenifitForDiffDutyCycles];
 
-plot(Capa', costBenifitForDiffCapa, 'r', 'LineWidth',2);
-title('Average Electric Bill Cost Reduction(%) with Different Battery Capacities');
-xlabel('Battery Capacity (kWh)');
+plot(periods', costBenifitForDiffDutyCycles, 'r', 'LineWidth',2);
+title('Average Electric Bill Cost Reduction(%) with Different Shiftable Load Periods');
+xlabel('Shiftable Load Period (h)');
 ylabel('Cost Reduction (%) ');
 grid
 set(gcf, 'PaperPosition', [0 0 5 5]); %Position plot at left hand corner with width 5 and height 5.
 set(gcf, 'PaperSize', [5 5]); %Set the paper to have width 5 and height 5.
-saveas(gcf, '../simResults/storageBenifit', 'pdf') %Save figure
+saveas(gcf, '../simResults/shiftBenifit', 'pdf') %Save figure
 
-csvwrite('../simResults/storageSimResults.csv', storageSimResultData);
-
-        
+csvwrite('../simResults/shiftSimResults.csv', shiftSimResultData);
